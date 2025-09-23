d59208b1745c0e5e8497f42cddd466beba52da4b
Marina Gorokhova <marin-bojchenko@yandex.ru>
2025-09-23 10:15:38 +0300

    Commit_2

--- src/test/java/UnTests.java
+++ src/test/java/UnTests.java
@@ -14,7 +14,7 @@ public class UnTests extends BaseTest{
     @Test
     public void checkInvalidUsName() {
         loginPage.open();
-        loginPage.loginThruZip("standard_user");
+        loginPage.loginThruZip("locked_out_user");
         String errorMsg = browser.findElement(By.cssSelector("*[data-test='error']")).getText();
         assertEquals(errorMsg, "Epic sadface: Sorry, this user has been locked out.");
     }
